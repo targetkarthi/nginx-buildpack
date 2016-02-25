150b11bf06d02a0c2d4d68f96ecdc3d3dcf4b239
Donald.Armstrong2 <donald.armstrong@target.com>
2016-02-25 14:01:16 -0600

    rebuild on latest heroku

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,13 +9,12 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=1.5.11
-PCRE_VERSION=8.34
-HEADERS_MORE_VERSION=0.25
-
+NGINX_VERSION=1.9.12
+PCRE_VERSION=8.38
+HEADERS_MORE_VERSION=0.29
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+pcre_tarball_url=https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz
 headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
