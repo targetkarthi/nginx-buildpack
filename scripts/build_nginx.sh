6c9819de29eb4ed46e075aca8c6d0f83a1fcc4c1
Donnie Armstrong <donaldjarmstrong@gmail.com>
2016-02-26 07:59:33 -0600

    Update build_nginx.sh

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # Build NGINX and modules on Heroku.
-# This program is designed to run in a web dyno provided by Heroku.
-# We would like to build an NGINX binary for the builpack on the
-# exact machine in which the binary will run.
-# Our motivation for running in a web dyno is that we need a way to
-# download the binary once it is built so we can vendor it in the buildpack.
-#
-# Once the dyno has is 'up' you can open your browser and navigate
-# this dyno's directory structure to download the nginx binary.
 
 NGINX_VERSION=1.9.12
 PCRE_VERSION=8.38
@@ -19,24 +11,17 @@ headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-modu
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
-#echo "Serving files from /tmp on $PORT"
-#cd /tmp
-#python -m SimpleHTTPServer $PORT &
-
 cd $temp_dir
 echo "Temp dir: $temp_dir"
 
-echo "Downloading $nginx_tarball_url"
-curl -L $nginx_tarball_url | tar xvz
-echo "DONE!"
+echo "Downloading and extracting $nginx_tarball_url"
+(cd $temp_dir; curl -L# $nginx_tarball_url | tar xvz)
 
-echo "Downloading $pcre_tarball_url"
-(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvz )
-echo "DONE!"
+echo "Downloading and extracting $pcre_tarball_url"
+(cd nginx-${NGINX_VERSION}; curl -L# $pcre_tarball_url | tar xvz)
 
-echo "Downloading $headers_more_nginx_module_url"
-(cd nginx-${NGINX_VERSION} && curl -L $headers_more_nginx_module_url | tar xvz )
-echo "DONE!"
+echo "Downloading and extracting $headers_more_nginx_module_url"
+(cd nginx-${NGINX_VERSION}; curl -L# $headers_more_nginx_module_url | tar xvz)
 
 (
 	cd nginx-${NGINX_VERSION}
@@ -47,9 +32,3 @@ echo "DONE!"
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
 	make install
 )
-
-while true
-do
-	sleep 1
-	echo "."
-done
