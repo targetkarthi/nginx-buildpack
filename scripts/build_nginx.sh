af2888e4655ebc3b84539aa6622435036687caac
Donnie Armstrong <donaldjarmstrong@gmail.com>
2016-02-26 08:20:55 -0600

    Update build_nginx.sh

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -5,23 +5,21 @@ NGINX_VERSION=1.9.12
 PCRE_VERSION=8.38
 HEADERS_MORE_VERSION=0.29
 
-nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz
-headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
+NGINX_TARBALL_URL=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+PCRE_TARBALL_URL=https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz
+HEADERS_MORE_NGINX_MODULE_TARBALL_URL=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
 
-temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
+BUILD_DIR=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
+echo "Build dir: $BUILD_DIR"
 
-cd $temp_dir
-echo "Temp dir: $temp_dir"
+echo "Downloading and extracting $NGINX_TARBALL_URL"
+(cd $BUILD_DIR; curl -L# $NGINX_TARBALL_URL | tar xz)
 
-echo "Downloading and extracting $nginx_tarball_url"
-(cd $temp_dir; curl -L# $nginx_tarball_url | tar xz)
+echo "Downloading and extracting $PCRE_TARBALL_URL"
+(cd nginx-${NGINX_VERSION}; curl -L# $PCRE_TARBALL_URL | tar xz)
 
-echo "Downloading and extracting $pcre_tarball_url"
-(cd nginx-${NGINX_VERSION}; curl -L# $pcre_tarball_url | tar xz)
-
-echo "Downloading and extracting $headers_more_nginx_module_url"
-(cd nginx-${NGINX_VERSION}; curl -L# $headers_more_nginx_module_url | tar xz)
+echo "Downloading and extracting $HEADERS_MORE_NGINX_MODULE_TARBALL_URL"
+(cd nginx-${NGINX_VERSION}; curl -L# $HEADERS_MORE_NGINX_MODULE_TARBALL_URL | tar xz)
 
 (
 	cd nginx-${NGINX_VERSION}
@@ -32,3 +30,6 @@ echo "Downloading and extracting $headers_more_nginx_module_url"
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
 	make install
 )
+
+(cp /tmp/nginx/sbin/nginx ../bin)
+(cp /tmp/nginx/conf/mime.types ../config)
